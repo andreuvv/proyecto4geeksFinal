2f1f7029d482a2736fcf0fdee405dc6fcb0823d8
andryandroid <andjos27@gmail.com>
2020-01-23 18:32:14 -0300

    [agregado view home]

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Login } from './views/login';
+import { Home } from './views/home';
 //import { BrowserRouter, Route, Switch } from "react-router-dom";
 //import injectContext from "./store/appContext";
 
@@ -7,7 +8,7 @@ import { Login } from './views/login';
 function Layout() {
   return (
     <>
-      <Login />
+      < Login />
     </>
   );
 }

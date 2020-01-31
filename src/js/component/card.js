b603b67905ebb36dc0dc490819ad2c4636ab9a92
andreuvv <veanve@gmail.com>
2020-01-31 18:33:25 -0300

    Card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -6,10 +6,13 @@ export const Card = () => {
         <div className="card" style={{ width: 100 + "%" }}>
             <img src="..." className="card-img-top" alt="..."></img>
             <div className="card-body">
-                <h5 className="card-title">Título</h5>
-                <p className="card-text">Descrión</p>
+                <h4 className="card-title">Título</h4>
+                <br></br>
+                <h6 className="card-subtitle text-muted float-left">Creador</h6>
+                <h6 id="estado" className="card-subtitle text-success float-right">Estado</h6>
+                <br></br>
+                <p className="card-text d-flex">Descripción</p>
                 <a href="#" id="sumarse" className="btn btn-success">Sumarse</a>
-                <a href="#" id="bajarse" className="btn btn-danger">Bajarse</a>
             </div>
         </div>
     );

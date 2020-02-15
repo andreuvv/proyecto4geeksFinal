24b7daf1164d163aa49e1a8e540b05378c7d147b
andryandroid <andjos27@gmail.com>
2020-02-14 21:22:19 -0300

    [primer fech listo]

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,12 +1,36 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			
+			usuario: {},
+			eventos: []
 		},
 		actions: {
-			
+			getUsers: (url, propiedad) => {
+				fetch(url, {
+					method: "GET",
+					headers:{
+						"Content-Type": "application/json"
+					}
+					
+				})
+					.then(resp => {
+						return resp.json(); 
+					})
+					.then(data => {
+						console.log(data); 
+						var obj = data;
+						setStore({ [propiedad]: obj });
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			}
 		}
 	};
 };
 
 export default getState;
+
+
+
+		
